e0ca38efe50039e139fbb441025850b5d133ff5a
Oleh ANTONENKO <oantonen@e2r7p9.unit.ua>
2018-12-05 19:31:39 +0200

    task01 done

--- task01.cpp
+++ task01.cpp
@@ -1,22 +1,34 @@
-
+#include <iostream>
 
 class Person {
 private:
-	constexpr LIMIT = 25;
-	string mLastName; // Person's last name
+	constexpr static int LIMIT = 25;
+	std::string mLastName; // Person's last name
 	char mFirstName[LIMIT]; // Person's first name
 public:
 	Person() {
-	mLastName = ""; 
-	mFirstName[0] = '\0'; 
-} // #1
-	Person(const string & ln, const char * fn = "Heyyou") {
-	mLastName 
+		mLastName = ""; 
+		mFirstName[0] = '\0'; 
+	} // #1
+	Person(const std::string & ln, const char * fn = "Heyyou") {
+		mLastName = ln;
+		if (fn != nullptr) {
+			int i = 0;
+			while (i < LIMIT - 1 && fn[i]) {
+				mFirstName[i] = fn[i];
+				i++;
+			}
+			mFirstName[i] = '\0';
+		}
+	}
  // #2
 // the following methods display mLastName and mFirstName
-	void Show() const; // firstname lastname format
-	void FormalShow() const; // lastname, firstname format
+	void Show() const // firstname lastname format
+	{
+		std::cout << mFirstName << ' ' << mLastName << std::endl;
+	}
+	void FormalShow() const // lastname, firstname format
+	{
+		std::cout << mLastName << ", " << mFirstName << std::endl;
+	}
 };
-
-
-
